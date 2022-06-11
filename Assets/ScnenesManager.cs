e33d16ac1243363fb925a1af20c1c77828d5d787
UsagihimeDenka <philippe8910@gmail.com>
2022-06-12 00:55:00 +0800

    Finish Game Loop

---
+++ Assets/ScnenesManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScnenesManager : MonoBehaviour
+{
+    public void ChangeScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+}
