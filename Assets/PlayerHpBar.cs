3ee7fe0c89ca15bb35258cdded6d8151ee08fc42
philippe <philippe8910@gmail.com>
2022-06-11 15:56:46 +0800

    added screen effect

--- Assets/PlayerHpBar.cs
+++ Assets/PlayerHpBar.cs
@@ -1,18 +1,40 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
 
 public class PlayerHpBar : MonoBehaviour
 {
+    public Slider hpSlider;
+
+    public RawImage screemEffect;
+
+    public VideoPlayer videoPlayer;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Hurt(0.1f);
+        }
+    }
+
+    public void Hurt(float _value)
+    {
+        hpSlider.value -= _value;
+
+        var a = 1 - hpSlider.value;
+        var m_color = screemEffect.color;
+        m_color.a = a;
         
+        videoPlayer.SetDirectAudioVolume(1 , a);
+        screemEffect.color = m_color;
     }
 }
